2f0d03ddaa2153820948498642e981d353113670
Jeri Fey <jericfey@gmail.com>
2020-10-23 23:17:58 -0600

    Able to post initials to the screen

--- assets/script1.js
+++ assets/script1.js
@@ -1,46 +1,24 @@
-const submit = document.querySelector(".initials-submit");
-const initialsList = document.querySelector(".initials");
-const initialsInput = document.querySelector(".initials-input");
+var initialsInput = document.querySelector("#initials-input");
+var initialsList = document.querySelector("#initials-list");
+var submit = document.querySelector("#sign-up");
 
-function template(data) {
-  initialsList.insertAdjacentHTML(
-    "beforeend",
-    `
-  <div class="initials flex items-start justify-start">
-      <div class="flex-1">
-        <p class="initials-body">${data.initials}</p>
-      </div>
-    </div>
-  </div>`
-  );
-}
 
-function appendInitials(event) {
-  const data = {
-    initials: initialsInput.value,
-  };
-
+//event listener for submit button
+submit.addEventListener("click", function(event) {
   event.preventDefault();
-  // If the value is nothing just return
-  if (initialsInput.value.length < 1) return;
-
-  // Insert new template into DOM
-  template(data);
-  console.log(data);
-
-  // Reset textarea value
-  initialsInput.value = "";
-
-  // Save the list to localStorage
-  localStorage.setItem("initialsListing", initialsList.innerHTML);
-}
-
-// submit.addEventListener("click", appendInitials, false);
-
-// Check for saved wishlist items
-const saved = localStorage.getItem("initialsListing");
-
-// If there are any saved items, update our list
-if (saved) {
-  initialsList.innerHTML = saved;
-}
+  
+  //create user object from submission
+  var user = {
+    initials: initialsInput.value.trim()
+  }
+  
+     
+      // set new submission
+      console.log(user);
+      localStorage.setItem("user", JSON.stringify(user));
+      
+      // get most recent submission
+      var lastWinner = localStorage.getItem("user");
+      lastWinner = JSON.parse(lastWinner);
+      initialsList.textContent = lastWinner.initials;
+  });
